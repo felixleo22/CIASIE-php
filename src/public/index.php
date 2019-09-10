e233937e79751cec483e414643a606323e6d1485
Hugo Jahnke <hugo.jahnke@gmail.com>
2019-09-10 11:40:34 +0200

    Modification du traitement de l'URL

--- src/public/index.php
+++ src/public/index.php
@@ -5,10 +5,14 @@ require_once '../../vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-
 $app = new \Slim\App;
 
-$app->get('/[hello[/{name}]]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args){
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/hello[/{name}]', function (Request $request, Response $response, array $args) {
     if (isset($args['name'])){
         $name = $args['name'];
         $response->getBody()->write("Hello, ${name}!");
