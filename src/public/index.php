9c74ad7130ffcf56b98ec2f0a5bff7886e0b0359
felixleo22 <leofelixoff@outlook.fr>
2019-09-10 10:39:13 +0200

    premier commit / installation slim

---
+++ src/public/index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once '../../vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+$app = new \Slim\App;
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->run();
+?>
