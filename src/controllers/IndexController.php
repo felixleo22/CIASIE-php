ed4e8d063a46a1c5c3976df4eebe70650e320806
Hugo Jahnke <hugo.jahnke@gmail.com>
2019-09-17 15:36:26 +0200

    Création du controlleur Root

--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -7,10 +7,11 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-
-class TestController
+class IndexController
 {
-
-    public function index(Request $request, Response $response, array $args){
+    public function index(Request $request, Response $response){
+        $response->getBody()->write("Hello world!");
+        return $response;
     }
+
 }
