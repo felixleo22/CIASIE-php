a53be87db41ee3e6f49b3fb1cdd353b7a93de679
Hugo Jahnke <hugo.jahnke@gmail.com>
2019-09-17 15:23:16 +0200

    Test d'affichage d'un template Twig en passant par un controlleur

---
+++ src/controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace MyApp\controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+
+class TestController
+{
+
+    public function index(Request $request, Response $response, array $args){
+        $response->getBody()->write("Hello");
+        return $response;
+    }
+}
