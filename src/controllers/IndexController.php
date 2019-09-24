3ecf823a43b5bb0365b4e34f491734d98b6cc4d6
Hugo Jahnke <hugo.jahnke@gmail.com>
2019-09-24 21:35:40 +0200

    Test sur IndexController

--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -3,6 +3,7 @@
 
 namespace MyApp\controllers;
 
+use MyApp\models\Entite;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
@@ -10,7 +11,10 @@ use Slim\Views\Twig;
 class IndexController extends Controller
 {
     public function index(Request $request, Response $response){
-        $this->views->render($response, 'index.html.twig');
+        $this->views->render($response, 'index.html.twig', [
+            'user' => array('name' => "Toto"),
+            'path' => __DIR__
+        ]);
     }
 
 }
