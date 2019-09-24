e0edf8f84b2c22efe18696f0c27210a0d5686562
apinot <antoine.pinot1@gmail.com>
2019-09-24 11:17:03 +0200

    ajout de commentaires

--- src/controllers/Utils.php
+++ src/controllers/Utils.php
@@ -3,10 +3,13 @@ namespace MyApp\controls;
 
 class Utils {
     
+    /**
+     * Permet de generer un nom de fichier et de le deplacer dans le bon dossier
+     */
     public static function uploadFichier($directory, UploadedFileInterface $uploadedFile)
     {
         $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
-        $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
+        $basename = bin2hex(time().random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
         $filename = sprintf('%s.%0.8s', $basename, $extension);
     
         $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
