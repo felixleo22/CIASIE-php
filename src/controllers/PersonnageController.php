c203e65e55be9842e9fdb5cc3affa09754f40a8f
Hugo Jahnke <hugo.jahnke@gmail.com>
2019-09-24 09:32:13 +0200

    Correction erreur nom de classe PersonnageController

--- src/controllers/PersonnageController.php
+++ src/controllers/PersonnageController.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-class Personnage extends Controller
+class PersonnageController extends Controller
 {
     public function Create(Request $request, Response $response, $args){
         return $this->views->render($response, 'ajoutPersonnage.html.twig');
