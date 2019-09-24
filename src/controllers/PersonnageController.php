0e9f44e1e2aeb8dc873e420089904e9af8526869
felixleo22 <leofelixoff@outlook.fr>
2019-09-24 10:29:53 +0200

    methode post pour l'ajout d'un personnage dans la bdd

--- src/controllers/PersonnageController.php
+++ src/controllers/PersonnageController.php
@@ -13,16 +13,20 @@ class PersonnageController extends Controller
         return $this->views->render($response, 'ajoutPersonnage.html.twig');
     }
 
+    /**
+     * l'attribut photo n'est pas gere pour le moment (initialise a vide)
+     */
     public function creerPersonnage(Request $request, Response $response, $args){
         $personnage = [];
         $personnage['nom'] = $request->getParsedBodyParam('nom');
         $personnage['prenom'] = $request->getParsedBodyParam('prenom');
         $personnage['type'] = $request->getParsedBodyParam('type');
         $personnage['taille'] = $request->getParsedBodyParam('taille');
-        $personnage['pointAgi'] = $request->getParsedBodyParam('pointAgi');
+        $personnage['pointVie'] = $request->getParsedBodyParam('pointVie');
         $personnage['pointAtt'] = $request->getParsedBodyParam('pointAtt');
         $personnage['pointDef'] = $request->getParsedBodyParam('pointDef');
         $personnage['pointAgi'] = $request->getParsedBodyParam('pointAgi');
+        $personnage['photo'] = "";
         $entite = Entite::create($personnage);
     }
 }
