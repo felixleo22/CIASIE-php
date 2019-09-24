263c8e9629187f0fb23ce97878d911bb7b1a4630
Hugo Jahnke <hugo.jahnke@gmail.com>
2019-09-24 08:27:15 +0200

    Création du SuperController

---
+++ src/controllers/SuperController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace MyApp\controllers;
+
+
+class SuperController
+{
+    private $c = null;
+    protected $views = null;
+
+    public function __construct($container)
+    {
+        $this->c = $container;
+        $this->views = $container["view"];
+    }
+
+}
