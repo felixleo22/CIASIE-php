e8b1bc1e30a82fe7fd2beb3a42708344ca7d974a
Hugo Jahnke <hugo.jahnke@gmail.com>
2019-10-02 16:50:28 +0200

    Ajout de la twigFunction get_admin_login

--- src/config/twigFunctions.inc.php
+++ src/config/twigFunctions.inc.php
@@ -5,4 +5,5 @@ use Smash\controllers\FlashMessage;
 return [
     new Twig_Function("est_connecte", Auth::class."::estConnecte"),
     new Twig_Function("get_message", FlashMessage::class."::get"),
+    new Twig_Function("get_admin_login", Auth::class."::getAdminLogin")
 ];
