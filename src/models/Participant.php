cd833c1b6434630cca9f83f27606c6bc7be63007
apinot <antoine.pinot1@gmail.com>
2019-10-15 09:16:14 +0200

    model participant

--- src/models/Participant.php
+++ src/models/Participant.php
@@ -5,5 +5,16 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Participant extends Model {
-    
+    use SoftDeletes;
+    protected $table = 'participantCombat';
+    protected $fillable = ['pointVie', 'nbAttaqueInflige', 'nbAttaqueRecu', 'degatInflige', 'degatRecu', 'gagner'];
+    public $timestamps = true;
+
+    public function combat() {
+        $this->belongsTo('Combat', 'idCombat');
+    }
+
+    public function entite() {
+        $this->belongsTo('Entite', 'idEntite');
+    }
 }
