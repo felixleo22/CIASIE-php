2249cd49d305e38c0f281aa0140b39858ce8890e
apinot <antoine.pinot1@gmail.com>
2019-10-29 14:55:47 +0100

    preparation de l'affichage pour defense/ attaque

--- src/public/js/combat.js
+++ src/public/js/combat.js
@@ -4,25 +4,46 @@ $('document').ready(() => {
     const participant1PV = $('#participant1PV');
     const participant2PV = $('#participant2PV');
     const gameMessage = $('#gameMessage');
-
+    
     //formulaire
     const playNextForm = document.getElementById('playNextForm');
     const submitBtn = $('#submitBtn');
     
+    const chooseActionForm = document.getElementById('chooseActionForm');
+    const attaquerBtn = $('#attaquerBtn');
+    const defendreBtn = $('#defendreBtn');
+    
+    //jouer un tour avec choix
+    defendreBtn.on('click', () => {
+        const url = chooseActionForm.getAttribute('action');
+        
+        play(url);
+    });
+    
+    attaquerBtn.on('click', () => {
+        const url = chooseActionForm.getAttribute('action');
+        
+        play(url);
+    });
+    
+    //jouer un tour normal
     playNextForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const url = playNextForm.getAttribute('action');
-        
+        play(url);
+    });   
+    
+    //requete ajax
+    function play(url) {
         fetch(url, {
             method: 'POST',
         })
         .then((response) => {
             if(!response.ok) {
-              //TODO faire qq chose en cas d'erreur
-              console.error(response);
-              return;
+                console.error(response);
+                return;
             }
-
+            
             return response.json();
         })
         .then((data) => {
@@ -31,20 +52,40 @@ $('document').ready(() => {
                 window.location.reload();
                 return;
             }
-
+            
             updateDisplay(pv1, pv2, typeOfNext, message);
         });
-    });   
-
+    }
+    
+    //mise a jour de l affichage
     function updateDisplay(pv1 , pv2,typeOfNext, message) {
         participant1PV.text(pv1);
         participant2PV.text(pv2);
         gameMessage.text(message);
-
-        if(typeOfNext === 'ended') {
+        
+        
+        switch (typeOfNext) {
+            case 'ended':
             submitBtn.val('Voir le résultat');
-        }else{
+            showPlayNextForm();
+            break;
+            case 'monstre':
             submitBtn.val('Jouer le prochain coup');
+            showPlayNextForm();                
+            break;
+            case 'personnage':
+            showChooseActionForm();
+            break; 
         }
+    }
+    
+    function showPlayNextForm() {
+        playNextForm.style.display = 'block';
+        chooseActionForm.style.display = 'none';        
+    }
+    
+    function showChooseActionForm() {
+        chooseActionForm.style.display = 'flex';
+        playNextForm.style.display = 'none';
     }
 });
