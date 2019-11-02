4dd87fe5e561ed443a6db18747c2ae31ebf64b7f
apinot <antoine.pinot1@gmail.com>
2019-11-02 16:02:24 +0100

    selection de 1 ou 3 perso en fonction du mode

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,55 +1,67 @@
 /**
- * Type de combat a créer (1v1 ou 3v3);
- */
+* Type de combat a créer (1v1 ou 3v3);
+*/
 let currentMode = '1v1';
 
 /**
- * Personnages séléctionnées
- */
-let currentPersoChecked = [];
+* Personnages séléctionnées
+*/
+let currentPersoChecked = 0;
 
 /**
- * Monstres séléctionés
- */
-let currentMonsterChecked = [];
+* Monstres séléctionés
+*/
+let currentMonsterChecked = 0;
 
 /**
- * Utilité : Permet à l'utilisateur de visualiser le choix d'un perso
- * @param type - Le type de perso concerné
- * @param id - L'ID du perso sélectionné
- */
-function border(type, id) {
-    const cards = $("#deck-" + type + " div.card");
-    cards.css("border-color", "");
-
-    const card = $("#card-" + id);
-
-    card.css("border-color", "dodgerblue");
-}
-
-/**
- * Utilité : Coche/Décoche des checkboxes
- *  On décoche toutes les checkboxes d'un type de perso
- *  On coche la checkbox associée au perso sélectionné
- * @param type - Le type de perso concerné
- * @param id - L'ID du perso sélectionné
- */
-function checkboxes(type, id) {
-    const checkboxes = $("#deck-" + type + " input.check");
-    checkboxes.prop("checked", false);
-
-    const checkbox = $("input#" + id + ".check");
-    checkbox.prop("checked", true);
-}
-
-/**
- * Utilité : Appelle les fonctions qui s'occupent du choix des persos
- * @param type - Le type de perso concerné
- * @param id - L'ID du perso sélectionné
- */
+* Utilité : Permet de selectionner ou déselectioner une entite
+* @param id - L'ID du perso sélectionné
+*/
 function select(type, id) {
-    checkboxes(type, id);
-    border(type, id);
+    //selection ou deselection
+    const checkbox = $("input#" + id + ".check");
+    const card = $("#card-" + id);
+    const isChecked = checkbox.prop("checked");
+    
+    if(isChecked) {
+        checkbox.prop("checked", false);
+        card.css("border-color", "");
+        if(type === 'monstre') {
+            currentMonsterChecked--;
+        }else{
+            currentPersoChecked--;
+        }
+    }else {
+        if(!couldCheck(type)) return;
+        checkbox.prop("checked", true);
+        card.css("border-color", "dodgerblue");
+        if(type === 'monstre') {
+            currentMonsterChecked++;
+        }else{
+            currentPersoChecked++;
+        }
+    }
+}
+
+function couldCheck(type) {
+    if(type === 'personnage') {
+        if(currentMode === '3v3') {
+            return currentPersoChecked < 3;
+        }
+        else {
+            return currentPersoChecked < 1;
+        }
+    }
+    
+    if(type === 'monstre') {
+        if(currentMode === '3v3') {
+            return currentMonsterChecked < 3;
+        }
+        else {
+            return currentMonsterChecked < 1;
+        }
+    }
+    return false;
 }
 
 function toggleStats(target){
@@ -62,30 +74,28 @@ function toggleStats(target){
         target = "Perdant";
     }
     $("input#btn-show-stats")
-        .val("Afficher les stats du " + target)
-        .data("target", target);
+    .val("Afficher les stats du " + target)
+    .data("target", target);
 }
 
 $(document).ready(function () {
     /**
-     * Detecte le changement du combat en 1v1 ou 3v3
-     */
+    * Detecte le changement du combat en 1v1 ou 3v3
+    */
     $('input.selectCombatMode[type=radio]').change(function() {
         // on change le mode
-       if(!this.checked) return;
-       currentMode = this.value;
-
-       //on reset la selection
-       currentMonsterChecked = [];
-       currentPersoChecked = [];
-
-       //on reset l'affichage de la selection
-       const checkboxes = $(`input.check`);
-       checkboxes.prop("checked", false);
-       const cards = $("div.card.selectable");
-       cards.css("border-color", "");
-
-
+        if(!this.checked) return;
+        currentMode = this.value;
+        
+        //on reset la selection
+        currentMonsterChecked = 0;
+        currentPersoChecked = 0;
+        
+        //on reset l'affichage de la selection
+        const checkboxes = $(`input.check`);
+        checkboxes.prop("checked", false);
+        const cards = $("div.card.selectable");
+        cards.css("border-color", "");
     });
     
     $("input.btn-select").click(function () {
@@ -93,10 +103,10 @@ $(document).ready(function () {
         const id = $(this).prop("id");
         select(type, id);
     });
-
+    
     $("input#btn-show-stats").click(function () {
         const target = $(this).data("target");
         toggleStats(target);
     })
-
+    
 });
